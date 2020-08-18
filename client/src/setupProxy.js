46ebf63676dda338f3d891f31622e2a13c6bf9f6
Eva Maina <evajohnson714@gmail.com>
2020-08-18 12:15:39 +0300

    developing the client side

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,5 +1,19 @@
-const proxy = require('http-proxy-middleware');
 
+const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = function(app) {
-  app.use(proxy('/auth/google', { target: 'http://localhost:5000/' }));
+  app.use(
+    '/auth/google',
+    createProxyMiddleware({
+      target: 'http://localhost:5000',
+      changeOrigin: true,
+    })
+  );
+
+  app.use(
+    '/api/*',
+    createProxyMiddleware({
+      target: 'http://localhost:5000',
+      changeOrigin: true,
+    })
+  );
 };
